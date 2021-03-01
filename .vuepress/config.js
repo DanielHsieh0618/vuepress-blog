8fee39dbb452e21c8d4dd65d083a489419892710
daniel <dan2y006@gmail.com>
2021-03-02 01:24:54 +0800

    change diretories

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'My VuePress Project',
+    description: `A simple VuePress project deployed with github.`,
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guides/' },
+      ],
+    },
+    dest: 'public',
+  }

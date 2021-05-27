b60c2e677a3c0b56e34c88df826693f347be9c0f
daniel <dan2y006@gmail.com>
2021-05-28 02:38:38 +0800

    update PWA manifest config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,12 +2,15 @@ module.exports = {
   title: "Daniel Hsieh' blog",
   description: `A simple VuePress project deployed with github.`,
   head: [
-    // ['link', {
-    //   rel: 'stylesheet',
-    //   href: `https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css`
-    // }],
-    // ['script', { src: `https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js` }],
-    // ['script', { src: `https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js` }],
+    ['link', { rel: 'icon', href: '/icon-192x192.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icon-192x192.png' }],
+    ['link', { rel: 'mask-icon', href: '/icon-256x256.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icon-192x192.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
     nav: [
@@ -37,4 +40,10 @@ module.exports = {
     ]
   },
   dest: 'public',
+  plugins: [
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: true
+    }]
+  ]
 }
